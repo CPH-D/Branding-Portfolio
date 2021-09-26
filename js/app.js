9028dd95cec1f744f7e51315f5de7bf8d00e6b52
Christopher Hernandez <cerberean661@gmail.com>
2021-09-26 02:36:15 -0700

    fixed header destinations for portfolio pages

--- js/app.js
+++ js/app.js
@@ -1,15 +1,3 @@
-// var menuBtn = document.getElementsByClassName('menu-btn')
-// var mobileMenu = document.getElementsByClassName('mobile-menu')
-
-// var clickedBtn = function () {
-//     mobileMenu[0].classList.toggle('active')
-// }
-
-// menuBtn[0].addEventListener('click', clickedBtn)
-
-// console.log(menuBtn[0])
-
-
 
 window.addEventListener("scroll", function () {
     var header = this.document.querySelector("header");
